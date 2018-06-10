712847c92a5e273f4c07d2c8cfcac34a9a555d2c
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-06-10 21:31:09 +0200

    modified PostActions.js

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -5,6 +5,8 @@ export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const EDIT_POST = 'EDIT_POST';
 export const DELETE_POST = 'DELETE_POST';
+export const THUMB_UP = 'THUMB_UP';
+export const THUMB_DOWN = 'THUMB_DOWN';
 
 // Export Actions
 export function addPost(post) {
@@ -70,6 +72,27 @@ export function editPostRequest(cuid, post) {
 export function deletePost(cuid) {
   return {
     type: DELETE_POST,
+    cuid,
+  };
+}
+
+export function deletePost(cuid) {
+  return {
+    type: DELETE_POST,
+    cuid,
+  };
+}
+
+export function thumbUp(cuid) {
+  return {
+    type: THUMB_UP,
+    cuid,
+  };
+}
+
+export function thumbDown(cuid) {
+  return {
+    type: THUMB_DOWN,
     cuid,
   };
 }
