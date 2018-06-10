dab9c1eacfe943559d87830c7182d0e4ea3a7252
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-06-10 18:19:37 +0200

    Init-dev-22_6

--- client/modules/About/About.js
+++ client/modules/About/About.js
@@ -2,20 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+// Import Style
+import styles from './About.css';
+
 class About extends Component {
-	render() {
-		return (
-			<div>
-				<h2>About us</h2>
-				<p>Lorem ipsum</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+    	<div>
+    		<h2>About us</h2>
+    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    			Nullam in leo nec erat tincidunt convallis sed a libero. Sed id.
+    		</p>
+    	</div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-	return {};
-}
+const mapStateToProps = (state) => {
+  return {};
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {};
